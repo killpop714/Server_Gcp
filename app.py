7188a0f50aa86be4b817e6c2306cfcde1a1772b4
killpop714 <112853297+killpop714@users.noreply.github.com>
2025-05-01 20:58:51 +0900

    a

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import jwt, datetime
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
