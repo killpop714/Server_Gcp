e21fae1a431cae61e766f30db6587d91567b248b
killpop714 <112853297+killpop714@users.noreply.github.com>
2025-05-02 19:33:39 +0900

    test

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, jsonify, request
-import jwt, datetime
-from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite://messages.db'
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'your-secret-key'
 
 user_data = {}
+
+
 actsuccess ={
 	"Draw":"1111",
 	"Chat":"1234",
 	"See":"123"
 }
+
+messages=[]
 
 
 @app.route('/localact', methods=['POST'])
@@ -29,9 +34,19 @@ def localact():
 
 	return jsonify(success=True)
 
+@app.route('/post_message', methods=['POST'])
+def post_message():
+	data = request.get_json()
+	messages.append({'username':data.get('username'),'text':data.get('text')})
+	return jsonify({'status': 'ok'})
+
+@app.route('/get_messages')
+def	get_messages():
+	return jsonify(messages)
+
 @app.route('/')
 def home():
-	return render_template("page.html")
+	return render_template("test.html")
 
 if __name__ == "__main__":
 	app.run("0.0.0.0",port=5000)
