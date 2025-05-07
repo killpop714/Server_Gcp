67aa28745e02e6fc6b933cd6a69577de29f16117
killpop714 <112853297+killpop714@users.noreply.github.com>
2025-05-07 18:52:07 +0900

    카메라 기능 추가와 대부분 CSS 수정

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ actsuccess ={
 messages=[]
 
 
-@app.route('/localact', methods=['POST'])
+@app.route('/localact_qr', methods=['POST'])
 def localact():
 	print(request.method)
 	data = request.get_json()
 	user_id = data.get('user_id')
 	act = data.get('act')
-	password = data.get('password')
+	password = data.get('code')
 	if actsuccess.get(act) != password:
 		return jsonify(success=False)
 	
@@ -46,7 +46,7 @@ def	get_messages():
 
 @app.route('/')
 def home():
-	return render_template("page.html")
+	return render_template("test2.html")
 
 if __name__ == "__main__":
 	app.run("0.0.0.0",port=5000)
