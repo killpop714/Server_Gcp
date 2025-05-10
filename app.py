b0a7c6f0639e854e7dde9a5279d0b5b1c84bab18
killpop714 <112853297+killpop714@users.noreply.github.com>
2025-05-10 19:10:23 +0900

    d

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def verify():
 #기본 사이트
 @app.route('/')
 def home():
-	return render_template("animation.html")
+	return render_template("page.html")
 
 if __name__ == "__main__":
 	app.run("0.0.0.0",port=5000)
