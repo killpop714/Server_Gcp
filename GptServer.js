b178e12b27a065772d2e93e78698c42bf067a081
killpop714 <112853297+killpop714@users.noreply.github.com>
2025-07-04 14:22:04 +0900

    ㅇ

--- GptServer.js
+++ GptServer.js
@@ -6,6 +6,11 @@ app.get('/',(req,res)=>{
     res.send("돌아가는 중");
 })
 
+app.post('/chat',(req,res)=>{
+    console.log(`받은 메세지: ${JSON.stringify(req.body)}`)
+    res.json({reply:'응답 테스트 완료'})
+})
+
 app.listen(port,()=>{
     console.log("듣고 있어요")
 })
